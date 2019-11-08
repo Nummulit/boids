c87028748bfd91da8042a94ac4280ef66a8d52d7
Kajetan Bochajczuk <kbochajczuk@gmail.com>
2019-11-08 15:09:03 +0100

    Code cleaning. Unnecessary comments removed.

--- game-of-life/gameOfLife.js
+++ game-of-life/gameOfLife.js
@@ -1,12 +1,10 @@
-const width = 30;
-const height = 30;
+const NROWS = 25;
+const NCOLS = 50;
+const ALIVEPROP = 0;
 
-// const gameField = document.getElementById('game-field');
-
-const randomInt = (min, max) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
-
+/*
+ * Implementation of the matrix using simple array.
+ */
 class Matrix {
   constructor(nrows, ncols, element = (x, y) => undefined) {
     this.nrows = nrows;
@@ -29,7 +27,9 @@ class Matrix {
   }
 }
 
-
+/*
+ * Field representing the game field.
+ */
 class Field {
   constructor(nrows, ncols, cells, tableRows = undefined) {
     this.nrows = nrows;
@@ -150,7 +150,7 @@ const nextBtn = document.getElementById('next');
 const autoRunBtn = document.getElementById('auto-run');
 const table = document.getElementById('game-field');
 
-let field = Field.random(20, 15, 0);
+let field = Field.random(NROWS, NCOLS, ALIVEPROP);
 field.draw(table);
 
 nextBtn.addEventListener('click', (event) => {
@@ -178,95 +178,3 @@ autoRunBtn.addEventListener('click', (event) => {
     }, 200);
   }
 });
-
-
-
-// // This hold the checkboxes that display the grid in the document
-// let cells = [];
-// for (let row = 0; row < height; row++) {
-//   let row = document.createElement('tr');
-//   gameField.appendChild(row);
-//   for (let col = 0; col < width; col++) {
-//     let cell = document.createElement('td');
-//     cell.classList += "cell";
-//     cell.dataset.alive = false;
-//     cell.addEventListener('click', (event) => {
-//       if (event.target.style.backgroundColor === 'green') {
-//         event.target.style.backgroundColor = `rgba(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)}, 0.2)`;
-//         event.target.dataset.alive = false;
-//       } else {
-//         event.target.style.backgroundColor = 'green';
-//         event.target.dataset.alive = true;
-//       }
-//     });
-//     // Just for fun
-//     cell.style.backgroundColor = `rgba(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)}, 0.2)`;
-//     row.appendChild(cell);
-//     cells.push(cell);
-//   }
-// }
-
-//
-// function gridFromCheckboxes() {
-//   return checkboxes.map(box => box.checked);
-// }
-//
-// function checkboxesFromGrid(grid) {
-//   grid.forEach((value, i) => checkboxes[i].checked = value);
-// }
-//
-// function randomGrid() {
-//   let result = [];
-//   for (let i = 0; i <width * height; i++) {
-//     result.push(Math.random() < 0.3);
-//   }
-//   return result;
-// }
-//
-// checkboxesFromGrid(randomGrid());
-//
-// function countNeighbors(grid, x, y) {
-//   let count = 0;
-//   for (let y1 = Math.max(0, y - 1); y1 <= Math.min(height - 1, y + 1); y1++) {
-//     for (let x1 = Math.max(0, x - 1); x1 <= Math.min(width - 1, x + 1); x1++) {
-//       if ((x1 !== x || y1 !== y) && grid[x1 + y1 * width]) {
-//         count++;
-//       }
-//     }
-//   }
-//   return count;
-// }
-//
-// function nextGeneration(grid) {
-//   let newGrid = new Array(width * height);
-//   for (let y = 0; y < height; y++) {
-//     for (let x = 0; x < width; x++) {
-//       let neighbors = countNeighbors(grid, x, y);
-//       let offset = x + y * width;
-//       if (neighbors < 2 || neighbors > 3) {
-//         newGrid[offset] = false;
-//       } else if (neighbors === 2) {
-//         newGrid[offset] = grid[offset];
-//       } else {
-//         newGrid[offset] = true;
-//       }
-//     }
-//   }
-//   return newGrid;
-// }
-//
-// function turn() {
-//   checkboxesFromGrid(nextGeneration(gridFromCheckboxes()));
-// }
-//
-// document.querySelector("#next").addEventListener("click", turn);
-//
-// let running = null;
-// document.querySelector("#run").addEventListener("click", () => {
-//   if (running) {
-//     clearInterval(running);
-//     running = null;
-//   } else {
-//     running = setInterval(turn, 400);
-//   }
-// });
