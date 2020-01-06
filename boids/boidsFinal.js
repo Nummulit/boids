703c19121e0ab2bf5032e23d0300ffa187cbd3ad
Kajetan Bochajczuk <kbochajczuk@gmail.com>
2020-01-06 13:54:00 +0100

    MVP

---
+++ boids/boidsFinal.js
@@ -0,0 +1,169 @@
+class Vec2D {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  copy() {
+    return new Vec2D(this.x, this.y);
+  }
+
+  add(vec) {
+    return new Vec2D(this.x + vec.x, this.y + vec.y);
+  }
+
+  subtract(vec) {
+    return new Vec2D(this.x - vec.x, this.y - vec.y);
+  }
+
+  multiply(scalar) {
+    return new Vec2D(this.x * scalar, this.y * scalar);
+  }
+
+  divide(scalar) {
+    return new Vec2D(this.x / scalar, this.y / scalar);
+  }
+
+  dot(vec) {
+    return this.x * vec.x + this.y * vec.y;
+  }
+
+  norm() {
+    return Math.sqrt(this.dot(this));
+  }
+}
+
+class Boid {
+  constructor(pos, vel) {
+    this.pos = pos;
+    this.vel = vel;
+  }
+
+  neighbours(boids, radius) {
+    return boids.filter(
+      boid => this !== boid && this.pos.subtract(boid.pos).norm() < radius
+    );
+  }
+
+  update(boids) {
+    this.vel = this.vel
+      .add(this.cohesion(this.neighbours(boids, 100)))
+      .add(this.separation(this.neighbours(boids, 20)))
+      .add(this.alignment(this.neighbours(boids, 100)));
+
+    const maxSpeed = 3;
+    if (this.vel.norm() > maxSpeed) {
+      this.vel = this.vel.divide(this.vel.norm()).multiply(maxSpeed);
+    }
+
+    this.pos = this.pos.add(this.vel);
+  }
+
+  cohesion(neighbours) {
+    if (neighbours.length === 0) return new Vec2D(0, 0);
+
+    let centerPos = new Vec2D(0, 0);
+    for (let boid of neighbours) {
+      centerPos = centerPos.add(boid.pos);
+    }
+    centerPos = centerPos.divide(neighbours.length);
+    const coefficient = 0.001;
+    return centerPos.subtract(this.pos).multiply(coefficient);
+  }
+
+  separation(neighbours) {
+    if (neighbours.length === 0) return new Vec2D(0, 0);
+
+    let separationVel = new Vec2D(0, 0);
+    for (let boid of neighbours) {
+      separationVel = separationVel.subtract(boid.pos.subtract(this.pos));
+    }
+    const coefficient = 0.1;
+    return separationVel.multiply(coefficient);
+  }
+
+  alignment(neighbours) {
+    if (neighbours.length === 0) return new Vec2D(0, 0);
+
+    let alignmentVel = new Vec2D(0, 0);
+    for (let boid of neighbours) {
+      alignmentVel = alignmentVel.add(boid.vel);
+    }
+
+    alignmentVel = alignmentVel.divide(neighbours.length);
+    const coefficient = 0.01;
+    return alignmentVel.subtract(this.vel).multiply(coefficient);
+  }
+}
+
+class Field {
+  constructor(context) {
+    this.context = context;
+    this.canvas = context.canvas;
+  }
+
+  clear() {
+    this.context.fillStyle = "rgba(0, 0, 0, 1)";
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  draw(boids) {
+    for (let boid of boids) {
+      // Bouncing of the edges
+      if (boid.pos.x < 10) {
+        boid.pos.x = 10;
+        boid.vel.x *= -1;
+      }
+      if (boid.pos.y < 10) {
+        boid.pos.y = 10;
+        boid.vel.y *= -1;
+      }
+      if (boid.pos.x > this.canvas.width - 10) {
+        boid.pos.x = this.canvas.width - 10;
+        boid.vel.x *= -1;
+      }
+      if (boid.pos.y > this.canvas.height - 10) {
+        boid.pos.y = this.canvas.height - 10;
+        boid.vel.y *= -1;
+      }
+
+      this._drawSingleBoid(boid);
+    }
+  }
+
+  _drawSingleBoid(boid) {
+    this.context.beginPath();
+    this.context.arc(boid.pos.x, boid.pos.y, 10, 0, 2 * Math.PI);
+    this.context.fillStyle = "rgba(0, 255, 0, 0.5)";
+    this.context.fill();
+    this.context.closePath();
+  }
+}
+
+// Generating boids
+function randomBoids(N) {
+  const boids = [];
+  for (let i = 0; i < N; i++) {
+    boids.push(
+      new Boid(
+        new Vec2D(Math.random() * 600, Math.random() * 600),
+        new Vec2D(Math.random() * 2 - 1, Math.random() * 2 - 1)
+      )
+    );
+  }
+  return boids;
+}
+
+// DOM handling
+const field = new Field(document.getElementById("field").getContext("2d"));
+const boids = randomBoids(100);
+function updateCanvas() {
+  field.clear();
+  field.draw(boids);
+  for (let boid of boids) {
+    boid.update(boids);
+  }
+  window.requestAnimationFrame(updateCanvas);
+}
+
+updateCanvas();
